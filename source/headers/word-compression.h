3ab1420ddcc5f110e095344eb611db12701cb195
Lucas Fernando Amorim <lf.amorim@yahoo.com.br>
2019-01-28 02:53:31 -0200

    uppercase

---
+++ source/headers/word-compression.h
@@ -0,0 +1,21 @@
+#ifndef WORD_COMPRESSION_LIB_DEFINED
+#define WORD_COMPRESSION_LIB_DEFINED 1
+
+#include "./callbacks.h"
+#include "./types.h"
+#include "./codes.h"
+#include "./create-index.h"
+#include "./decoder.h"
+#include "./dictionary-binary-tree.h"
+#include "./dictionary-compare.h"
+#include "./dictionary-generator.h"
+#include "./dictionary.h"
+#include "./dictionary-iterator.h"
+#include "./file.h"
+#include "./filter.h"
+#include "./memory-management.h"
+#include "./pinch-string.h"
+#include "./position-index.h"
+#include "./word-reader.h"
+
+#endif
